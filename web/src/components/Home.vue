10327b56cd2b22dad58bbffda39c42f118dd2ed0
Zhai Can <bczhc0@126.com>
2024-10-13 11:57:21 +0800

    Home: fix empty link names and in-place `splice` call

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,22 +1,14 @@
 <script setup lang="ts">
 import {useRouter} from "vue-router";
 import {routes} from "../router.ts";
+
 let router = useRouter();
 </script>
 
 <template>
   <n-ul>
-    <n-li v-for="r in routes.splice(1)">
-      <n-a @click="router.push(r.path)">{{
-          (() => {
-            let filePath = r.component['__file'];
-            if (!filePath) return 'Unknown';
-            let split = filePath.split('/');
-            let basename = split[split.length - 1];
-            let i = basename.lastIndexOf('.vue');
-            return basename.substring(0, i);
-          })()
-        }}</n-a>
+    <n-li v-for="r in [...routes].splice(1)">
+      <n-a @click="router.push(r.path)">{{ r.name }}</n-a>
     </n-li>
   </n-ul>
 </template>
